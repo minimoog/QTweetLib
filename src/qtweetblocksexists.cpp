f2a957cf0a793bdb7c220d956a5f2c642500c005
minimoog <minimoog77@gmail.com>
2011-03-25 01:23:09 +0100

    Added class for checking if user id blocking target user.

---
+++ src/qtweetblocksexists.cpp
@@ -0,0 +1,113 @@
+/* Copyright (c) 2011, Antonie Jovanoski
+ *
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact e-mail: Antonie Jovanoski <minimoog77_at_gmail.com>
+ */
+
+#include "qtweetblocksexists.h"
+#include <QtDebug>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include "qtweetuser.h"
+#include "qtweetconvert.h"
+
+/**
+ *  Constructor
+ */
+QTweetBlocksExists::QTweetBlocksExists(QObject *parent) :
+    QTweetNetBase(parent)
+{
+}
+
+/**
+ *  Constructor
+ *  @param oauthTwitter OAuthTwitter object
+ *  @param parent parent QObject
+ */
+QTweetBlocksExists::QTweetBlocksExists(OAuthTwitter *oauthTwitter, QObject *parent) :
+    QTweetNetBase(oauthTwitter, parent)
+{
+}
+
+/**
+ *  Checks if target user is blocked
+ *  @param userid The ID of the potentially blocked user.
+ *  @param includeEntities When set to true, each tweet will include a node called "entities"
+ */
+void QTweetBlocksExists::isBlocked(qint64 userid, bool includeEntities)
+{
+    if (!isAuthenticationEnabled()) {
+        qCritical("Needs authentication to be enabled");
+        return;
+    }
+
+    QUrl url("http://api.twitter.com/1/blocks/exists.json");
+
+    url.addQueryItem("user_id", QString::number(userid));
+
+    if (includeEntities)
+        url.addQueryItem("include_entities", "true");
+
+    QNetworkRequest req(url);
+
+    QByteArray oauthHeader = oauthTwitter()->generateAuthorizationHeader(url, OAuth::GET);
+    req.setRawHeader(AUTH_HEADER, oauthHeader);
+
+    QNetworkReply *reply = oauthTwitter()->networkAccessManager()->get(req);
+    connect(reply, SIGNAL(finished()), this, SLOT(reply()));
+}
+
+/**
+ *  Checks if target user is blocked
+ *  @param screenName the screen name of the potentially blocked user
+ *  @param includeEntities When set to true, each tweet will include a node called "entities"
+ */
+void QTweetBlocksExists::isBlocked(const QString &screenName, bool includeEntities)
+{
+    if (!isAuthenticationEnabled()) {
+        qCritical("Needs authentication to be enabled");
+        return;
+    }
+
+    QUrl url("http://api.twitter.com/1/blocks/exists.json");
+
+    url.addQueryItem("screen_name", screenName);
+
+    if (includeEntities)
+        url.addQueryItem("include_entities", "true");
+
+    QNetworkRequest req(url);
+
+    QByteArray oauthHeader = oauthTwitter()->generateAuthorizationHeader(url, OAuth::GET);
+    req.setRawHeader(AUTH_HEADER, oauthHeader);
+
+    QNetworkReply *reply = oauthTwitter()->networkAccessManager()->get(req);
+    connect(reply, SIGNAL(finished()), this, SLOT(reply()));
+}
+
+void QTweetBlocksExists::parsingJsonFinished(const QVariant &json, bool ok, const QString &errorMsg)
+{
+    if (ok) {
+        QTweetUser user = QTweetConvert::variantMapToUserInfo(json.toMap());
+
+        emit finishedIsBlocked(user);
+    } else {
+        qDebug() << "QTweetBlocksExists parser error: " << errorMsg;
+        setLastErrorMessage(errorMsg);
+        emit error(JsonParsingError, errorMsg);
+    }
+}
---
+++ src/qtweetblocksexists.h
@@ -0,0 +1,47 @@
+/* Copyright (c) 2011, Antonie Jovanoski
+ *
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact e-mail: Antonie Jovanoski <minimoog77_at_gmail.com>
+ */
+
+#ifndef QTWEETBLOCKSEXISTS_H
+#define QTWEETBLOCKSEXISTS_H
+
+#include "qtweetnetbase.h"
+
+/**
+ *  Emits finishedIsBlocked if the authenticating user is blocking a target user.
+ *  Will return the blocked user's object if a block exists,
+ *  and error with a HTTP 404 response code otherwise.
+ */
+class QTweetBlocksExists : public QTweetNetBase
+{
+    Q_OBJECT
+public:
+    explicit QTweetBlocksExists(QObject *parent = 0);
+    QTweetBlocksExists(OAuthTwitter *oauthTwitter, QObject *parent = 0);
+    void isBlocked(qint64 userid, bool includeEntities = false);
+    void isBlocked(const QString& screenName, bool includeEntities = false);
+
+signals:
+    void finishedIsBlocked(const QTweetUser& user);
+
+protected slots:
+    void parsingJsonFinished(const QVariant &json, bool ok, const QString &errorMsg);
+};
+
+#endif // QTWEETBLOCKSEXISTS_H
