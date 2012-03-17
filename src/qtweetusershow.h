406226bb655592ace2f80746366807fd52d9a15a
Toni Jovanoski <minimoog77@gmail.com>
2012-03-17 20:36:55 +0100

    QTweetUserShow now uses qt json.

--- src/qtweetusershow.h
+++ src/qtweetusershow.h
@@ -42,7 +42,7 @@ signals:
     void parsedUserInfo(const QTweetUser& userInfo);
 
 protected slots:
-    void parsingJsonFinished(const QVariant &json, bool ok, const QString &errorMsg);
+    void parseJsonFinished(const QJsonDocument &jsonDoc);
 };
 
 #endif // QTWEETUSERSHOW_H
