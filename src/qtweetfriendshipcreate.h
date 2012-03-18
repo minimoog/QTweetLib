ac00f3f4b6e81dd2c53737eee8f834fee1b2e946
Toni Jovanoski <minimoog77@gmail.com>
2012-03-18 16:20:59 +0100

    QTweetFriendshipCreate now uses qt json.

--- src/qtweetfriendshipcreate.h
+++ src/qtweetfriendshipcreate.h
@@ -44,7 +44,7 @@ signals:
     void parsedUser(const QTweetUser& user);
 
 protected slots:
-    void parsingJsonFinished(const QVariant &json, bool ok, const QString &errorMsg);
+    void parseJsonFinished(const QJsonDocument &jsonDoc);
 };
 
 #endif // QTWEETFRIENDSHIPCREATE_H
