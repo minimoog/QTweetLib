f9d3913d812ef7df3a76a16f563f9820c5609803
Toni Jovanoski <minimoog77@gmail.com>
2013-02-22 01:01:53 +0100

    Update QTweetDirectMessageDestroy to use 1.1 API

--- src/qtweetdirectmessagedestroy.cpp
+++ src/qtweetdirectmessagedestroy.cpp
@@ -53,17 +53,24 @@ void QTweetDirectMessageDestroy::destroyMessage(qint64 id, bool includeEntities)
         return;
     }
 
-    QUrl url(QString("http://api.twitter.com/1/direct_messages/destroy/%1.json").arg(id));
+    QUrl url("https://api.twitter.com/1.1/direct_messages/destroy.json");
+    QUrl urlQuery("https://api.twitter.com/1.1/direct_messages/destroy.json");
+
+    urlQuery.addQueryItem("id", QString::number(id));
 
     if (includeEntities)
-        url.addQueryItem("include_entities", "true");
+        urlQuery.addQueryItem("include_entities", "true");
 
     QNetworkRequest req(url);
 
-    QByteArray oauthHeader = oauthTwitter()->generateAuthorizationHeader(url, OAuth::DELETE);
+    QByteArray oauthHeader = oauthTwitter()->generateAuthorizationHeader(urlQuery, OAuth::POST);
     req.setRawHeader(AUTH_HEADER, oauthHeader);
+    req.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
 
-    QNetworkReply *reply = oauthTwitter()->networkAccessManager()->deleteResource(req);
+    QByteArray statusPost = urlQuery.toEncoded(QUrl::RemoveScheme | QUrl::RemoveAuthority | QUrl::RemovePath);
+    statusPost.remove(0, 1);
+
+    QNetworkReply *reply = oauthTwitter()->networkAccessManager()->post(req, statusPost);
     connect(reply, SIGNAL(finished()), this, SLOT(reply()));
 }
 
