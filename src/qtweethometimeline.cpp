3cd2ab51ee0f0deef45d034477d84ee8d3cf9f88
Toni Jovanoski <minimoog77@gmail.com>
2013-07-03 00:41:01 +0200

    Updated json generator file.

--- src/qtweethometimeline.cpp
+++ src/qtweethometimeline.cpp
@@ -1,81 +1,21 @@
-/* Copyright 2010 Antonie Jovanoski
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Contact e-mail: Antonie Jovanoski <minimoog77_at_gmail.com>
- */
-
-#include <QtDebug>
+#include "qtweethometimeline.h"
 #include <QNetworkRequest>
 #include <QNetworkReply>
 #include <QUrlQuery>
-#include <QJsonDocument>
-#include <QJsonArray>
-#include "qtweethometimeline.h"
 #include "qtweetstatus.h"
 #include "qtweetconvert.h"
+#include <QJsonDocument>
+#include <QJsonArray>
 
-/**
- *  Constructor
- */
-QTweetHomeTimeline::QTweetHomeTimeline(QObject *parent) :
-    QTweetNetBase(parent),
-    m_sinceid(0),
-    m_maxid(0),
-    m_count(0),
-    m_trimUser(false),
-    m_includeEntities(false),
-    m_excludeReplies(false),
-    m_contributorDetails(false)
+QTweetHomeTimeline::QTweetHomeTimeline(QObject *parent) : QTweetNetBase(parent)
 {
 }
 
-/**
- *  Constructor
- *  @param oauthTwitter OAuthTwitter object
- *  @param parent parent QObject
- */
-QTweetHomeTimeline::QTweetHomeTimeline(OAuthTwitter *oauthTwitter, QObject *parent) :
-    QTweetNetBase(oauthTwitter, parent),
-    m_sinceid(0),
-    m_maxid(0),
-    m_count(0),
-    m_trimUser(false),
-    m_includeEntities(false),
-    m_excludeReplies(false),
-    m_contributorDetails(false)
+QTweetHomeTimeline::QTweetHomeTimeline(OAuthTwitter *oauthTwitter, QObject *parent) : QTweetNetBase(oauthTwitter, parent)
 {
 }
 
-/**
- *   Start fetching home timeline
- *   @param sinceid fetches tweets with ID greater (more recent) then sinceid
- *   @param maxid fetches tweets with ID less (older) then maxid
- *   @param count number of tweets to fetch (up to 200)
- *   @param page page number
- *   @param skipUser true to include only status authors numerical ID
- *   @param includeEntities true to include a node called "entities"
- *   @param excludeReplies true to prevent replies from appearing in the returned timeline
- *   @param contributorDetails true to enhance the contributors element of the status response
- *   @remarks Setting parameter to default value will not be put in a querys
- */
-void QTweetHomeTimeline::fetch(qint64 sinceid,
-                               qint64 maxid,
-                               int count,
-                               bool trimUser,
-                               bool includeEntities,
-                               bool excludeReplies,
-                               bool contributorDetails)
+void QTweetHomeTimeline::fetch(qint64 since_id, qint64 max_id, int count, bool trim_user, bool exclude_replies, bool contributor_details, bool include_entities)
 {
     if (!isAuthenticationEnabled()) {
         qCritical("Needs authentication to be enabled");
@@ -85,26 +25,26 @@ void QTweetHomeTimeline::fetch(qint64 sinceid,
     QUrl url("https://api.twitter.com/1.1/statuses/home_timeline.json");
     QUrlQuery urlQuery;
 
-    if (sinceid != 0)
-        urlQuery.addQueryItem("since_id", QString::number(sinceid));
+    if (since_id != 0)
+        urlQuery.addQueryItem("since_id", QString::number(since_id));
 
-    if (maxid != 0)
-        urlQuery.addQueryItem("max_id", QString::number(maxid));
+    if (max_id != 0)
+        urlQuery.addQueryItem("max_id", QString::number(max_id));
 
     if (count != 0)
         urlQuery.addQueryItem("count", QString::number(count));
 
-    if (trimUser)
+    if (trim_user)
         urlQuery.addQueryItem("trim_user", "true");
 
-    if (includeEntities)
-        urlQuery.addQueryItem("include_entities", "true");
-
-    if (excludeReplies)
+    if (exclude_replies)
         urlQuery.addQueryItem("exclude_replies", "true");
 
-    if (contributorDetails)
+    if (contributor_details)
         urlQuery.addQueryItem("contributor_details", "true");
+
+    if (include_entities)
+        urlQuery.addQueryItem("include_entities", "true");
 
     url.setQuery(urlQuery);
 
@@ -117,17 +57,11 @@ void QTweetHomeTimeline::fetch(qint64 sinceid,
     connect(reply, SIGNAL(finished()), this, SLOT(reply()));
 }
 
-void QTweetHomeTimeline::get()
-{
-    fetch(m_sinceid, m_maxid, m_count, m_trimUser, m_includeEntities, m_excludeReplies, m_contributorDetails);
-}
-
 void QTweetHomeTimeline::parseJsonFinished(const QJsonDocument &jsonDoc)
 {
     if (jsonDoc.isArray()) {
         QList<QTweetStatus> statuses = QTweetConvert::jsonArrayToStatusList(jsonDoc.array());
 
-        emit parsedStatuses(statuses);
+        emit statusList(statuses);
     }
 }
-
--- src/qtweethometimeline.h
+++ src/qtweethometimeline.h
@@ -1,88 +1,23 @@
-/* Copyright 2010 Antonie Jovanoski
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Contact e-mail: Antonie Jovanoski <minimoog77_at_gmail.com>
- */
+//-----------------Generated file---------------
+
 
 #ifndef QTWEETHOMETIMELINE_H
 #define QTWEETHOMETIMELINE_H
 
 #include "qtweetnetbase.h"
 
-/**
- *   Fetches user home timeline
- */
 class QTWEETLIBSHARED_EXPORT QTweetHomeTimeline : public QTweetNetBase
 {
     Q_OBJECT
-    Q_PROPERTY(qint64 sinceID READ sinceID WRITE setSinceID)
-    Q_PROPERTY(qint64 maxID READ maxID WRITE setMaxID)
-    Q_PROPERTY(int count READ count WRITE setCount)
-    Q_PROPERTY(bool trimUser READ isTrimUser WRITE setTrimUser)
-    Q_PROPERTY(bool includeEntities READ isIncludeEntities WRITE setIncludeEntities)
-    Q_PROPERTY(bool excludeReplies READ isExcludeReplies WRITE setExcludeReplies)
-    Q_PROPERTY(bool contributorDetails READ isContributorsDetails WRITE setContributorsDetails)
-
 public:
     QTweetHomeTimeline(QObject *parent = 0);
     QTweetHomeTimeline(OAuthTwitter *oauthTwitter, QObject *parent = 0);
-    void fetch(qint64 sinceid = 0,
-               qint64 maxid = 0,
-               int count = 0,
-               bool trimUser = false,
-               bool includeEntities = false,
-               bool excludeReplies = false,
-               bool contributorDetails = false);
-    void get();
-
-    void setSinceID(qint64 sinceid) { m_sinceid = sinceid; }
-    qint64 sinceID() const { return m_sinceid; }
-
-    void setMaxID(qint64 maxid) { m_maxid = maxid; }
-    qint64 maxID() const { return m_maxid; }
-
-    void setCount(int count) { m_count = count; }
-    int count() const { return m_count; }
-
-    void setTrimUser(bool trimUser) { m_trimUser = trimUser; }
-    bool isTrimUser() const { return m_trimUser; }
-
-    void setIncludeEntities(bool includeEntities) { m_includeEntities = includeEntities; }
-    bool isIncludeEntities() const { return m_includeEntities; }
-
-    void setExcludeReplies(bool excludeReplies) { m_excludeReplies = excludeReplies; }
-    bool isExcludeReplies() const { return m_excludeReplies; }
-
-    void setContributorsDetails(bool contributorsDetails) { m_contributorDetails = contributorsDetails; }
-    bool isContributorsDetails() const { return m_contributorDetails; }
+    void fetch(qint64 since_id = 0, qint64 max_id = 0, int count = 0, bool trim_user = false, bool exclude_replies = false, bool contributor_details = false, bool include_entities = false);
 
 signals:
-    /** Emits hometimeline status list */
-    void parsedStatuses(const QList<QTweetStatus>& statuses);
+    void statusList(const QList<QTweetStatus>& statuses);
 
 protected slots:
     void parseJsonFinished(const QJsonDocument &jsonDoc);
-
-private:
-    // ### TODO: Use pimpl
-    qint64 m_sinceid;
-    qint64 m_maxid;
-    int m_count;
-    bool m_trimUser;
-    bool m_includeEntities;
-    bool m_excludeReplies;
-    bool m_contributorDetails;
 };
-
-#endif // QTWEETHOMETIMELINE_H
+#endif
